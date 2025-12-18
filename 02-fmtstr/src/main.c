ba2ca6fbab5669abc24a22eb8b9417f723f558fb
stiles <plox.stiles@pm.me>
2025-12-18 14:53:36 -0600

    add: work in progres for the 2nd challenge. Just copied first challenge and adjusted buffer size and the strings. Still need to create the python3 test script.

---
+++ 02-fmtstr/src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void win(void){
+	printf("\nCTF_FLAG{example_flag}\n");
+};
+
+void fail(void){
+	printf("\nfailure...\n");
+};
+
+int main(void) {
+
+	puts(
+		"02-fmtstr this challenge is similar to 01-stack, "
+		"the vulnerability is exactly the same but this time "
+		"it wont read in enough characters to actually overflow. "
+		"This one relys on using a format string exploit to put "
+		"more characters in then whats possible. reminder the buffer "
+		"is 40 bytes long but this will only read 20 bytes."
+	);
+
+	char input [40] = {0};
+	int changeme = 0;
+
+	printf("input > ");
+	fgets(input, 20, stdin);
+
+	if (changeme != 0) {
+		win();
+	} else {
+		fail();
+	}
+
+	printf("changeme: %d\n", changeme);
+	printf("input: %s\n", input);
+
+	return 0;
+}

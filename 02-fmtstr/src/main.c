81a3f4d3477387ac95664c279e65fc85e03404b8
stiles <plox.stiles@pm.me>
2025-12-18 18:40:11 -0600

    recreate the source of the challenge in order to show the stack and how you directly correlate with it, still wip tho

--- 02-fmtstr/src/main.c
+++ 02-fmtstr/src/main.c
@@ -9,30 +9,44 @@ void fail(void){
 	printf("\nfailure...\n");
 };
 
-int main(void) {
+int vuln(void) {
 
-	int changeme = 0;
+    int _A = 0x41414141;
+    int _B = 0x42424242;
+	int changeme = 0xdeadbeef;
 	char input [40] = {0};
-	puts(
-		"02-fmtstr this challenge is similar to 01-stack, "
-		"the vulnerability is exactly the same but this time "
-		"it wont read in enough characters to actually overflow. "
-		"This one relys on using a format string exploit to put "
-		"more characters in then whats possible. reminder the buffer "
-		"is 40 bytes long but this will only read 20 bytes."
-	);
+    int _C = 0x43434343;
+    int _D = 0x44444444;
 
+    const char *msg = "02-fmtstr, this challenge is similar to the first one.\n"
+        "However this one will not read in more bytes then the buffer allows.\n"
+        "This requires a format string exploit to write some bytes on the stack.\n";
+
+	puts(msg);
+
+    printf("------STACK ADDR------\n");
+    printf("address of _A: %p\n", &_A);
+    printf("address of _B: %p\n", &_B);
     printf("address of changeme: %p\n", &changeme);
+    printf("address of _C: %p\n", &_C);
+    printf("address of _D: %p\n", &_D);
+    printf("----------------------\n");
+
+    printf("------STACK VALS------\n");
+    printf("address of _A: %08x\n", _A);
+    printf("address of _B: %08x\n", _B);
+    printf("address of changeme: %08x\n", changeme);
+    printf("address of _C: %08x\n", _C);
+    printf("address of _D: %08x\n", _D);
+    printf("----------------------\n");
 
 	printf("input > ");
 
+    // TODO - create a limit on this buffer with fgets
 	gets(input);
-
-	printf("changeme: %d\n", changeme);
-	printf("input: ");
     printf(input);
 
-	if (changeme != 0) {
+	if (changeme != 0xdeadbeef) {
 		win();
 	} else {
 		fail();
@@ -40,3 +54,10 @@ int main(void) {
 
 	return 0;
 }
+
+int main(void){
+    vuln();
+    return 0;
+}
+
+
